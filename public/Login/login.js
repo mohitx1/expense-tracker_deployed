f68306c30e6ac5ce4dd76abb7cfc908021147b4c
Mohit kumar <122800500+mohitx1@users.noreply.github.com>
2024-05-17 16:38:26 +0530

    url updated

--- public/Login/login.js
+++ public/Login/login.js
@@ -8,7 +8,7 @@ function login(e) {
 
     }
     console.log(loginDetails)
-    axios.post('https://expense-tracker-deployed.onrender.com/user/login',loginDetails).then(response => {
+    axios.post('http://localhost:3000/user/login',loginDetails).then(response => {
             alert(response.data.message)
             console.log(response.data)
             localStorage.setItem('token', response.data.token)
